68ccd86392af5eee4fbeb4d95c8a0a9d920e9ee3
Abdul Moiz <moiz.webevis@gmail.com>
2021-11-15 14:17:13 +0500

    check git

---
+++ src/components/OurMenu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const OurMenu=()=> {
+    return (
+     <section className="Our-Menu">
+        <div className="container">
+            
+        </div>
+     </section>
+    )
+}
+
+export default OurMenu
---
+++ src/components/OurMenusSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OurMenusSection() {
+    return (
+        <div className="">
+            
+        </div>
+    )
+}
+
+export default OurMenusSection
